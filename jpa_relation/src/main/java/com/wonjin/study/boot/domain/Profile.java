960ba0a09788a1b97d094d6f01e28d3b216762b5
Wonjin-Lee <claude17@naver.com>
2021-10-11 16:50:41 +0900

    연관관계 미적용 상태의 Member, Profile 엔티티 클래스 생성

---
+++ jpa_relation/src/main/java/com/wonjin/study/boot/domain/Profile.java
@@ -0,0 +1,27 @@
+package com.wonjin.study.boot.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString(exclude = "member")
+@Entity
+@Table(name = "tb1_profile")
+@EqualsAndHashCode(of = "fname")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fno;
+
+    private String fname;
+
+    private boolean current;
+
+    private Member member;
+}
