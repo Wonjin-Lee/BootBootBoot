0d8530dd5e99404a278ccd47de86ea6c28ff1330
Wonjin Lee <claude17@naver.com>
2021-10-13 01:27:06 +0900

    Update 처리 시에 순수 객체를 이용하는 방식 테스트 추가

--- jpa_relation/src/test/java/com/wonjin/study/boot/repository/PDSBoardRepositoryTest.java
+++ jpa_relation/src/test/java/com/wonjin/study/boot/repository/PDSBoardRepositoryTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
@@ -49,5 +51,32 @@ public class PDSBoardRepositoryTest {
 
         int count = repo.updatePDSFile(fno, newName);
         log.info("update count : " + count);
+    }
+
+    @Transactional
+    @Test
+    public void testUpdateFileName2() {
+        String newName = "updateFile2.doc";
+
+        Optional<PDSBoard> result = repo.findById(2L);
+        
+        result.ifPresent(pds -> {
+            log.info("데이터가 존재하므로 update 시도");
+
+            PDSFile target = new PDSFile();
+            target.setFno(2L);
+            target.setPdsfile(newName);
+
+            int idx = pds.getFiles().indexOf(target);
+
+            if (idx > -1) {
+                List<PDSFile> list = pds.getFiles();
+                list.remove(idx);
+                list.add(target);
+                pds.setFiles(list);
+            }
+
+            repo.save(pds);
+        });
     }
 }
