08a88990f590f2cc65e1cb50cdbacc068c8c3e64
Wonjin Lee <claude17@naver.com>
2021-10-17 16:37:18 +0900

    Thymeleaf를 이용한 프론트엔드 개발 프로젝트 생성

---
+++ front/src/test/java/com/wonjin/study/boot/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjin.study.boot.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
