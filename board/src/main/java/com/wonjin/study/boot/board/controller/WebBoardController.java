25488d308b81c13313331e4a901c5d365c286eee
Wonjin Lee <claude17@naver.com>
2021-10-20 23:08:47 +0900

    게시물 리스트 조회 시 Value Object를 이용한 Paging 처리 적용

--- board/src/main/java/com/wonjin/study/boot/board/controller/WebBoardController.java
+++ board/src/main/java/com/wonjin/study/boot/board/controller/WebBoardController.java
@@ -1,7 +1,14 @@
 package com.wonjin.study.boot.board.controller;
 
+import com.wonjin.study.boot.board.domain.WebBoard;
+import com.wonjin.study.boot.board.repository.WebBoardRepository;
+import com.wonjin.study.boot.board.vo.PageVO;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/boards/")
 @Log
 public class WebBoardController {
+
+    @Autowired
+    private WebBoardRepository webBoardRepository;
+
     @GetMapping("/list")
-    public void list() {
-        log.info("list() called...");
+    public void list(PageVO vo, Model model) {
+        Pageable pageable = vo.makePageable(0, "bno");
+
+        Page<WebBoard> result = webBoardRepository.findAll(webBoardRepository.makePredicate(null, null), pageable);
+
+        log.info("" + pageable);
+        log.info("" + result);
+
+        model.addAttribute("result", result);
     }
 }
